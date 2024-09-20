dd7c6fe05f55e9a2b086b86f6c404108f168b8be
marcosviana91 <marcos.viana.91@gmail.com>
2024-09-20 12:14:17 -0300

    feat - plataforma and alpha_1.0.1 description

--- player_site/urls.py
+++ player_site/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.site, name="player_site"),
+    path('alpha_1_0_1/', views.alpha_1_0_1, name="alpha_1_0_1"),
 ]
--- player_site/views.py
+++ player_site/views.py
@@ -3,6 +3,10 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
-@login_required(login_url="/users/login")
+# @login_required(login_url="/users/login")
 def site(request: HttpRequest):
+    # print(request.user.id)
     return render(request, 'plataforma.html')
+
+def alpha_1_0_1(request: HttpRequest):
+    return render(request, 'versions/alpha_1.0.1.html')
