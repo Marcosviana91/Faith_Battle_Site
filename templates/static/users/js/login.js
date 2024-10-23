72c03ee861d8c12721d59b7f66b3017f37521f5d
marcosviana91 <marcos.viana.91@gmail.com>
2024-10-22 23:43:33 -0300

    style - login form, create user form, avatar selector, navigation ok

--- templates/static/users/js/login.js
+++ templates/static/users/js/login.js
@@ -1,13 +1,39 @@
+// Change background
 const login_bg_list = $('#login_bg')[0].dataset['src'].split(';')
 var current_bg_index = 0
-
 setInterval(function() {
     if (current_bg_index < login_bg_list.length-1) {
         current_bg_index +=1
     } else {
         current_bg_index = 0
     }
-    console.log(login_bg_list[current_bg_index])
     $('#login_bg')[0].src = login_bg_list[current_bg_index]
 }
 ,10000)
+
+const form_login = $('#form_login')
+const form_create_user = $('#form_create_user')
+const avatar_container = $('#avatar_container')
+
+function showLoginForm() {
+    form_login.show()
+    form_create_user.hide()
+    avatar_container.hide()
+}
+
+function showCreateForm() {
+    form_login.hide()
+    form_create_user.show()
+    avatar_container.hide()
+}
+
+function showAvatarForm() {
+    form_login.hide()
+    form_create_user.hide()
+    avatar_container.show()
+}
+
+$('.avatar_selector').on("click", function(e) {
+    console.log(e.currentTarget.dataset)
+    showCreateForm()
+})
