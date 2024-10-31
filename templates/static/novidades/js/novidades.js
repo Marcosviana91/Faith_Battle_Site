b8e2486f103a4427fabbe4d1298904467d23cdfb
marcosviana91 <marcos.viana.91@gmail.com>
2024-10-30 22:11:21 -0300

    fix - cards show HTML tags in text

--- templates/static/novidades/js/novidades.js
+++ templates/static/novidades/js/novidades.js
@@ -1,6 +1,5 @@
 // Filtro
 var filtro = ''
-
 $('input').on('input', function (e) {
     e.preventDefault();
     filtro = String(e.target.value).toLowerCase()
@@ -20,6 +19,13 @@ const indicators = $('.carousel-indicators button')
 indicators[0].classList.add("active")
 for (let indicator = 0 ;  indicator < indicators.length ; indicator++) {
     indicators[indicator].dataset['bsSlideTo'] = indicator
+}
+
+// Transform o conteúdo de uma elemento .toInnerHtml
+const toInnerHtml = $('.toInnerHtml')
+for (element of toInnerHtml) {
+    console.log(element.innerText)
+    element.innerHTML = element.innerText
 }
 
 
